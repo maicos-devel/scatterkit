4374c417af5d359929e289a85b06d6922b29cd64
hejamu <info@jaegerhenrik.de>
2025-11-04 11:59:50 +0100

    Initial reading

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 #
 # Released under the GNU Public Licence, v3 or any higher version
 # SPDX-License-Identifier: GPL-3.0-or-later
-"""Setup file for MAICoS package.
+"""Setup file for scatterkit package.
 
 Credit to MDAnalysis setup.py.
 """
