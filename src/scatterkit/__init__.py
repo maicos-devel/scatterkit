add07ed1ce6873c010b4a606325f67fe66b69869
Henrik Stooss <info@jaegerhenrik.de>
2026-03-19 14:57:20 +0100

    Remove versioneer (#16)
    * Remove versioneer
    
    * Remove versioneer
    
    * Changelog
    
    * Changelog
    
    * Clarify setup.py behaviour
    
    * lint
    
    * Cleanup
    
    * Remove platform dependency
    
    * Fix bash
    
    * Don't track `_version.py` since it is now build time

--- src/scatterkit/__init__.py
+++ src/scatterkit/__init__.py
@@ -2,14 +2,11 @@
 
 __authors__ = "MAICoS Developer Team"
 
-from ._version import get_versions
+#: Version information for Scatterkit, following :pep:`440`
+#: and `semantic versioning <http://semver.org/>`_.
+from ._version import __version__  # noqa: F401
 from .diporderstructurefactor import DiporderStructureFactor
 from .rdfdiporder import RDFDiporder
 from .saxs import Saxs
-
-#: Version information for Scatterkit, following :pep:`440`
-#: and `semantic versioning <http://semver.org/>`_.
-__version__ = get_versions()["version"]
-del get_versions
 
 __all__ = ["Saxs", "DiporderStructureFactor", "RDFDiporder"]
